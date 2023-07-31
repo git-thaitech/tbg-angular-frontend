243715d730891cc15c0462db39b753a8bf8d9088
dev2 <dev2@techeese.net>
2023-07-31 14:17:24 +0700

    31/07/2023 2:17 PM

--- src/app/components/create-mortgage-agreement/create-mortgage-agreement.component.ts
+++ src/app/components/create-mortgage-agreement/create-mortgage-agreement.component.ts
@@ -7,12 +7,16 @@ import { NumberToWordsPipe } from 'src/app/pipes/number-to-words.pipe';
   styleUrls: ['./create-mortgage-agreement.component.css']
 })
 export class CreateMortgageAgreementComponent implements OnInit {
-  
+
   number!: number;
   convertedMoneyString!: string;
   convertedGoldWeightUnit!: string;
   showExtendedCustomerInfo = false;
   customerName!: string;
+  itemRows: { itemName: string, goldType: string, itemRealWeight: any }[] = [];
+  itemName!: string;
+  goldType!: string;
+  itemRealWeight!: any;
 
   constructor(private numberToWordsPipe: NumberToWordsPipe) { }
 
@@ -24,6 +28,24 @@ export class CreateMortgageAgreementComponent implements OnInit {
     const number = parseFloat(value.replace(/[^0-9]/g, ''));
     this.convertedMoneyString = this.numberToWordsPipe.transform(number);
     console.log(this.convertedMoneyString);
+  }
+
+  addItemRow() {
+    console.log("haha");
+    console.log(this.itemName);
+    const row = {
+      itemName: this.itemName,
+      goldType: this.goldType,
+      itemRealWeight: this.itemRealWeight
+    };
+    this.itemRows.push(row);
+  }
+
+  removeRow(row: any) {
+    const index = this.itemRows.indexOf(row);
+    if (index > -1) {
+      this.itemRows.splice(index, 1);
+    }
   }
 
 
