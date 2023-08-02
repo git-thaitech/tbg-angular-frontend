7b48abb59c7a7f1a4361d7c39b71e75a6c9a687f
dev2 <dev2@techeese.net>
2023-08-02 12:30:15 +0700

    12:30 2/8/2023

--- src/app/components/create-order/create-order.component.ts
+++ src/app/components/create-order/create-order.component.ts
@@ -9,15 +9,30 @@ export class CreateOrderComponent implements OnInit {
 
   numberValue!: string;
   wordValue!: string;
+  isExchangeItem = false;
 
   constructor(private elementRef: ElementRef) { }
 
   ngOnInit(): void {
-
     var s = document.createElement("script");
     s.type = "text/javascript";
     s.src = "../assets/js/main.js";
     this.elementRef.nativeElement.appendChild(s);
+  }
+
+  oldItemRows: { 
+    oldItemType: string, 
+    oldItemExPrice: string, 
+    oldItemBuyPrice: string,
+    isExchangeItem: boolean,
+    itemTotalWeight: number,
+    itemGemWeight: number,
+    itemRealWeight: any }[] = [];
+
+  changeOldItemFunction() {
+    console.log("how are you");
+    this.isExchangeItem = !this.isExchangeItem;
+    console.log(this.isExchangeItem);
   }
 
 }
